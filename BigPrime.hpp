e2ee8a23d9021010c73b1831a97fa43e991fff3b
trioKun <42882626+trioKun@users.noreply.github.com>
2019-02-01 09:49:25 +0800

    Update BigPrime.hpp

--- BigPrime.hpp
+++ BigPrime.hpp
@@ -13,7 +13,6 @@ public:
 	BigInt operator()();                 // 调用返回一个大素数
 
 private:
-	uint64_t count = 0;     // 用于测试时计数
 	std::default_random_engine rand_eng;          // 随机数生成器，调用返回unsigned类型的随机值
 	BigInt big_rand();          // 生成1024位随机大奇数
 	bool check_evidence(const BigInt &, unsigned);              // 检查给定数是大数为合数的证据则返回true
